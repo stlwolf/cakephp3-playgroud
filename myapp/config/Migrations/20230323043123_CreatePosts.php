33dea055b25e17a467495aab3cb25d8e94d5f6f7
eddy <eddy@stlwolf.com>
2023-03-23 13:36:20 +0900

    fix: Reset all code

---
+++ myapp/config/Migrations/20230323043123_CreatePosts.php
@@ -0,0 +1,34 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreatePosts extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('posts');
+        $table->addColumn('title', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('body', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
